0dc78d054302286f85918896caf2bd0321a22922
rettekram <mark.etter@gmail.com>
2018-04-03 13:03:48 -0700

    Initial draft of Pasadena Kids Yoga page

---
+++ pasadena-kids-yoga/index.php
@@ -0,0 +1,70 @@
+<!doctype html>
+<html lang="en">
+	
+<head>
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <title>Pasadena Kid's Yoga</title>
+	<meta name="description" content="" />
+	<meta name="keywords" content="" />
+
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+	<script defer src="https://use.fontawesome.com/releases/v5.0.9/js/all.js" integrity="sha384-8iPTk2s/jMVj81dnzb/iFR2sdA7u06vHJyyLlAd4snFpCl/SnyUjRrbdJsw1pGIl" crossorigin="anonymous"></script>
+</head>
+
+<body>
+	<br />
+	<div class="container">
+
+		<div class="jumbotron">
+			<h1 class="display-1">Pasadena Kid’s Yoga</h1>
+			<p class="lead">Keyword rich description for Google.</p>
+		</div>
+		
+		<div class="row">
+			<div class="col-lg-3">
+				<h2>Why YOGA?</h2>
+				<p>Yoga helps with:</p>
+				<ul>
+					<li>Brain Development</li>
+					<li>Stress Management</li>
+					<li>Breath awareness</li>
+					<li>Focus</li>
+					<li>Strength and Flexibility</li>
+					<li>Immune System</li>
+				</ul>
+			</div>
+			<div class="col-lg-9">
+				<h2>WHY YOGA for Kids?</h2>
+				<p class="lead">Yoga gives kids the tools to manage stress and emotional waves appropriately. Whether the source of the stress is caused by navigating various social interactions, like starting preschool, or transitioning into elementary school, Yoga offers the student the ability to soothe anxious emotions through breath sensing and focusing on the breath. Yoga also offers kids the ability to gain confidence in new environments, to learn the art of focus and mindfulness to release emotional upsets, to gain strength, develop balance, and integrate the brain and the body through focused asana practice. Plus, it is a WHOLE LOT OF FUN!</p>
+			</div>
+		</div>
+		
+		<p><img class="img-fluid" src="http://via.placeholder.com/1200x400" alt="Banner Image"></p>
+
+		<h2>About PASADENA KID’S YOGA</h2>
+		<div class="card float-md-right" style="width: 18rem; margin-left: 10px;">
+			<img class="card-img-top" src="http://via.placeholder.com/300x200" alt="Pili Bailey">
+			<div class="card-body">
+				<h5 class="card-title">Pili Bailey</h5>
+				<p class="card-text">
+					<i class="fas fa-mobile-alt"></i> 626-578-5906<br />
+					<i class="fas fa-globe"></i>  <a href="http://www.essentialyogaspace.com" target="_blank">essentialyogaspace.com</a>
+				</p>
+				<p class="card-text"><a href="mailto:pili@pilibailey.com" class="btn btn-primary"><i class="fas fa-envelope"></i> Contact</a></p>
+			</div>
+		</div>
+		<p class="lead">Pasadena Kid’s Yoga began in 2015, as a vision that Pili Bailey, Pasadena Kid’s Yoga founder and creator, had to bring Yoga to kids from all races, ethnic groups, and socioeconomic backgrounds. After Pili became a mother for the first time in 2013, she began to feel the pull to get back to her center through her practice of yoga. This lead Pili to attend a 6 month long YogaWorks 200 hour teacher training in 2015, and to train to teach kids yoga through miniyogis (™) also in 2015.  Pili began teaching yoga to moms with tots through Fuller Seminary, and also taught kid’s yoga with friends at various neighborhood parks. As her son, now aged 4, grew, Pili incorporated yoga into their times together.  She saw how it inspired her son to have confidence in his own body, to increase his strength, and to gain a greater sense of boldness in navigating changes in his environment. As a science minded individual, with a Masters in Chemistry from Georgetown University, Pili keeps her knowledge current by studying scientific literature confirming the positive effects of yoga on the developing brain.</p>
+		<p class="lead">Pili has experience teaching yoga through Preschools to ages 3-6, to elementary ages 7-11. Pili has taught parent and me yoga, moms and tots yoga, and kids yoga throughout Glendale, Pasadena, South Pasadena, and San Marino. Combining games, songs, stories, and relaxation and mindfulness, Pili Bailey creates original and engaging lesson plans appropriate to the age group she is working with.</p>
+		<p class="lead">Pili Bailey works with preschools, city recreation centers, YMCA, and privately with families and small groups to bring yoga to kids throughout San Gabriel Valley.</p>
+		<p class="lead">Contact Pili Bailey today to schedule your private yoga lessons for your kids, or to bring Pili to your local school or recreation center.</p>
+
+	</div>
+	<br />
+	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</body>
+
+</html>
