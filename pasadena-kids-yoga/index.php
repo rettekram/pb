d4524fa628baf3ba2d73dff716da6b547e2d7b6e
rettekram <mark.etter@gmail.com>
2018-07-12 21:27:50 -0700

    Update fontawesome lib

--- pasadena-kids-yoga/index.php
+++ pasadena-kids-yoga/index.php
@@ -10,16 +10,16 @@
 	<meta name="keywords" content="" />
 
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-	<script defer src="https://use.fontawesome.com/releases/v5.0.9/js/all.js" integrity="sha384-8iPTk2s/jMVj81dnzb/iFR2sdA7u06vHJyyLlAd4snFpCl/SnyUjRrbdJsw1pGIl" crossorigin="anonymous"></script>
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous">
 </head>
 
 <body>
 	<br />
 	<div class="container">
 
-		<div class="jumbotron">
-			<h1 class="display-1">Pasadena Kid’s Yoga</h1>
-			<p class="lead">Keyword rich description for Google.</p>
+		<div class="jumbotron" style="background-color:#82BFAF;">
+			<h1 class="display-1" style="color:#fff;"><i class="fas fa-spa"></i> Pasadena Kid’s Yoga</h1>
+			<p class="lead" style="color:#fff;">Keyword rich description for Google.</p>
 		</div>
 		
 		<div class="row">
@@ -50,12 +50,13 @@
 				<h5 class="card-title">Pili Bailey</h5>
 				<p class="card-text">
 					<i class="fas fa-mobile-alt"></i> 626-578-5906<br />
-					<i class="fas fa-globe"></i>  <a href="http://www.essentialyogaspace.com" target="_blank">essentialyogaspace.com</a>
+					<i class="fas fa-globe"></i>  <a href="https://www.essentialyogaspace.com" target="_blank">essentialyogaspace.com</a>
 				</p>
 				<p class="card-text"><a href="mailto:pili@pilibailey.com" class="btn btn-primary"><i class="fas fa-envelope"></i> Contact</a></p>
 			</div>
 		</div>
-		<p class="lead">Pasadena Kid’s Yoga began in 2015, as a vision that Pili Bailey, Pasadena Kid’s Yoga founder and creator, had to bring Yoga to kids from all races, ethnic groups, and socioeconomic backgrounds. After Pili became a mother for the first time in 2013, she began to feel the pull to get back to her center through her practice of yoga. This lead Pili to attend a 6 month long YogaWorks 200 hour teacher training in 2015, and to train to teach kids yoga through miniyogis (™) also in 2015.  Pili began teaching yoga to moms with tots through Fuller Seminary, and also taught kid’s yoga with friends at various neighborhood parks. As her son, now aged 4, grew, Pili incorporated yoga into their times together.  She saw how it inspired her son to have confidence in his own body, to increase his strength, and to gain a greater sense of boldness in navigating changes in his environment. As a science minded individual, with a Masters in Chemistry from Georgetown University, Pili keeps her knowledge current by studying scientific literature confirming the positive effects of yoga on the developing brain.</p>
+
+		<p class="lead">Pasadena Kid’s Yoga began in 2015, as a vision that Pili Bailey, Pasadena Kid’s Yoga founder and creator, had to bring Yoga to kids from all races, ethnic groups, and socioeconomic backgrounds. After Pili became a mother for the first time in 2013, she began to feel the pull to get back to her center through her practice of yoga. This lead Pili to attend a 6 month long YogaWorks 200 hour teacher training in 2015, and to train to teach kids yoga through miniyogis&trade; also in 2015.  Pili began teaching yoga to moms with tots through Fuller Seminary, and also taught kid’s yoga with friends at various neighborhood parks. As her son, now aged 4, grew, Pili incorporated yoga into their times together.  She saw how it inspired her son to have confidence in his own body, to increase his strength, and to gain a greater sense of boldness in navigating changes in his environment. As a science minded individual, with a Masters in Chemistry from Georgetown University, Pili keeps her knowledge current by studying scientific literature confirming the positive effects of yoga on the developing brain.</p>
 		<p class="lead">Pili has experience teaching yoga through Preschools to ages 3-6, to elementary ages 7-11. Pili has taught parent and me yoga, moms and tots yoga, and kids yoga throughout Glendale, Pasadena, South Pasadena, and San Marino. Combining games, songs, stories, and relaxation and mindfulness, Pili Bailey creates original and engaging lesson plans appropriate to the age group she is working with.</p>
 		<p class="lead">Pili Bailey works with preschools, city recreation centers, YMCA, and privately with families and small groups to bring yoga to kids throughout San Gabriel Valley.</p>
 		<p class="lead">Contact Pili Bailey today to schedule your private yoga lessons for your kids, or to bring Pili to your local school or recreation center.</p>
@@ -63,7 +64,6 @@
 	</div>
 	<br />
 	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 </body>
 
